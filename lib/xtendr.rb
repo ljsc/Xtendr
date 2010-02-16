c97e0e7dae5f5e5f300d764b7bb65b2eac32f256
Lou Scoras <louis.j.scoras@gmail.com>
2010-02-16 10:14:43 -0500

    Updating docs

--- lib/xtendr.rb
+++ lib/xtendr.rb
@@ -10,6 +10,7 @@ end
 
 module Xtendr
   module FFI
+    include ::FFI
     extend ::FFI::Library
     ffi_lib 'System'
     attach_function 'setxattr', [:string, :string, :pointer, :int, :int, :int], :int
@@ -18,33 +19,65 @@ module Xtendr
     attach_function 'removexattr', [:string, :string, :int], :int
   end
 
+  # Get the value of an extended attribute for the current object.  Calls
+  # `#to_s` on self to get the path to the file the attribute is set on.
+  #
+  # @param [String] attribute The extended attribute to retrieve.
+  # @return [String, nil] The value of the attribute, or nil if it is not set.
+  #
   def getx(attribute)
     getx! attribute
   rescue Errno::ENOATTR
     nil
   end
 
+  # Get the value of an extended attribute, raising an error if the attribute is
+  # not found.
+  #
+  # The bang version of getx works the the same way as `getx`, exept that when
+  # the attribute is not found. In that case, rather than returning `nil`, an
+  # ENOATTR error is raised.
+  # @raise [Errno::ENOATTR] Describes the attribute that couldn't be located.
+  # @param [String] attribute The extended attribute to retrieve.
+  # @return [String] The value of the requested attribute.
+  #
   def getx!(attribute)
     len = FFI.getxattr(self.to_s, attribute, nil, 0, 0, 0)
-    raise_error(attribute, ::FFI::LastError::error) if len < 0
-    buffer = ::FFI::Buffer.alloc_out(len)
+    raise_error(attribute, FFI::LastError::error) if len < 0
+    buffer = FFI::Buffer.alloc_out(len)
     FFI.getxattr(self.to_s, attribute, buffer, buffer.size, 0, 0)
     buffer.get_string(0, buffer.size)
   end
 
+  # Set an extended attribute on a filesystem object.
+  # @param [String] attribute The extended attribute to retrieve.
+  # @param [String] value The value to set the extended attribute to.
+  # @param [Array<:create>] opts Optional flags which influence how setting is
+  #   done.
+  # @return [nil]
+  #
+  # *:create*:: Raises an error if the attribute already exists.
+  #
   def setx(attribute, value, *opts)
     options = opts.include?(:create) ? 0x02 : 0x00
     retval = FFI.setxattr(self.to_s, attribute, value, value.size, 0, options)
-    raise_error(nil, ::FFI::LastError::error) if retval < 0
+    raise_error(nil, FFI::LastError::error) if retval < 0
   end
 
+  # List all of the extended attribute for the object.
+  # @return [Array<String>] the list of all keys which have extended attributes
+  #   set.
   def listx
     len = FFI.listxattr(self.to_s, nil, 0, 0)
-    buffer = ::FFI::Buffer.alloc_out(len)
+    buffer = FFI::Buffer.alloc_out(len)
     FFI.listxattr(self.to_s, buffer, buffer.size, 0)
     buffer.get_bytes(0, buffer.size).split("\0")
   end
 
+  # Removes the given extended attribute from the filesystem object.
+  # @param [String] attribute The attribute to remove the extended attribute
+  #   for.
+  # @return [nil]
   def removex(attribute)
     FFI.removexattr(self.to_s, attribute, 0)
   end
