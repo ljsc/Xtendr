e0be586b84c9c9087ba3e1034ea0423af3d8b343
Lou Scoras <louis.j.scoras@gmail.com>
2010-02-22 09:16:46 -0500

    Adding xattr options support.

--- lib/xtendr.rb
+++ lib/xtendr.rb
@@ -19,8 +19,8 @@ module Xtendr
   # @return [String, nil] The value of the attribute, or nil if it is not set
   #                       and no block is passed.
   # @yield Block is run when the attribute is not found.
-  def getx(attribute)
-    getx! attribute
+  def getx(attribute, *opts)
+    getx! attribute, *opts
   rescue Errno::ENOATTR
     block_given? ? yield : nil
   end
@@ -35,28 +35,42 @@ module Xtendr
   # @param [String] attribute The extended attribute to retrieve.
   # @return [String] The value of the requested attribute.
   #
-  def getx!(attribute)
-    len = FFI.getxattr(self.to_s, attribute, nil, 0, 0, 0)
+  def getx!(attribute, *opts)
+    options = opts.include?(:no_follow) ? 1 : 0
+    len = FFI.getxattr(self.to_s, attribute, nil, 0, 0, options)
     raise_error(attribute, ::FFI::LastError::error) if len < 0
     buffer = ::FFI::Buffer.alloc_out(len)
-    FFI.getxattr(self.to_s, attribute, buffer, buffer.size, 0, 0)
+    FFI.getxattr(self.to_s, attribute, buffer, buffer.size, 0, options)
     buffer.get_string(0, buffer.size)
   end
 
   # Set an extended attribute on a filesystem object.
   # @param [String] attribute The extended attribute to retrieve.
   # @param [String] value The value to set the extended attribute to.
-  # @param [Array<:create>] opts Optional flags which influence how setting is
-  #   done.
+  # @param [Array<:create, :replace, :no_follow>] opts Optional flags which
+  #   influence how setting is done.
   # @return [nil]
   #
   # *:create*:: Raises an error if the attribute already exists.
   #
   def setx(attribute, value, *opts)
-    options = opts.include?(:create) ? 0x02 : 0x00
-    retval = FFI.setxattr(self.to_s, attribute, value, value.size, 0, options)
+    retval = FFI.setxattr(self.to_s, attribute, value, value.size, 0, setx_options(*opts))
     raise_error(nil, ::FFI::LastError::error) if retval < 0
   end
+
+  private
+    # Set the correct bitfields on the options flag for setting operations
+    # @param [Array<:replace, :create, :no_follow>] syms
+    # @private
+    def setx_options(*syms)
+      options = 0x00
+      options |= 0x04 if syms.include?(:replace)
+      options |= 0x02 if syms.include?(:create)
+      options |= 0x01 if syms.include?(:no_follow)
+      options
+    end
+
+  public
 
   # List all of the extended attribute for the object.
   # @return [Array<String>] the list of all keys which have extended attributes
@@ -76,6 +90,13 @@ module Xtendr
     FFI.removexattr(self.to_s, attribute, 0)
   end
 
+  # Remove all extended file attributes from the object
+  def remove_allx
+    listx.each do |attribute|
+      removex(attribute)
+    end
+  end
+
   protected
     def raise_error(msg, code)
       if code == Errno::ENOATTR::Errno
@@ -89,7 +110,7 @@ end
 class Errno::ENOATTR < SystemCallError
   # FIXME: This works on Mac OS X (Snow Leopard), but this really shouldn't be
   # hardcoded. Unfortunatly, we probably can't get the error number symbolically
-  # without a c extension, which defeats the purpose of usign ffi.
+  # without a C extension, which defeats the purpose of using ffi.
   Errno = 93
 
   def initialize(msg=nil)
--- test/test_xtendr.rb
+++ test/test_xtendr.rb
@@ -1,6 +1,17 @@
 require 'helper'
+require 'fileutils'
 
 class TestXtendr < Test::Unit::TestCase
+  include FileUtils
+
+  def setup
+    @store = Xtendr(__FILE__)
+  end
+
+  def teardown
+    Xtendr(__FILE__).remove_allx
+  end
+
   def test_module
     test_file = __FILE__
     test_file.extend(Xtendr)
@@ -9,57 +20,87 @@ class TestXtendr < Test::Unit::TestCase
   end
 
   def test_proxy
-    store = Xtendr(__FILE__)
-    store.setx('proxy', 'Foobar')
-    assert_equal 'Foobar', store.getx('proxy')
+    @store.setx('proxy', 'Foobar')
+    assert_equal 'Foobar', @store.getx('proxy')
   end
 
   def test_failing
-    store = Xtendr(__FILE__)
     assert_nothing_raised do
-      store.getx('missing')
+      @store.getx('missing')
     end
   end
 
   def test_failing_bang
-    store = Xtendr(__FILE__)
     assert_raises Errno::ENOATTR do
-      store.getx!('missing')
+      @store.getx!('missing')
     end
   end
 
   def test_get_default
-    store = Xtendr(__FILE__)
-    store.removex('removed') if store.getx('remove')
-    assert_equal 'default', store.getx('remove') { 'default' }
+    assert_equal 'default', @store.getx('remove') { 'default' }
   end
 
   def test_listing
-    store = Xtendr(__FILE__)
-    store.setx('listme', 'true')
-    store.setx('me_too', 'true')
-    assert store.listx.include?('listme')
-    assert store.listx.include?('me_too')
+    @store.setx('listme', 'true')
+    @store.setx('me_too', 'true')
+    assert @store.listx.include?('listme')
+    assert @store.listx.include?('me_too')
   end
 
   def test_removal
-    store = Xtendr(__FILE__)
-    store.setx('hide', 'true')
-    store.setx('seek', 'true')
-    store.removex('hide')
-    assert !store.listx.include?('hide')
-    assert store.listx.include?('seek')
+    @store.setx('hide', 'true')
+    @store.setx('seek', 'true')
+    @store.removex('hide')
+    assert !@store.listx.include?('hide')
+    assert @store.listx.include?('seek')
   end
 
-  def test_options
-    store = Xtendr(__FILE__)
-    store.removex('createme')
+  def test_remove_all
+    @store.setx('hide', 'true')
+    @store.setx('seek', 'true')
+    @store.remove_allx
+    assert !@store.listx.include?('hide')
+    assert !@store.listx.include?('seek')
+  end
+
+  def test_create_option
     assert_nothing_raised do
-      store.setx('createme', 'done!', :create)
+      @store.setx('createme', 'done!', :create)
     end
     assert_raise Errno::EEXIST do
-      store.setx('createme', 'done!', :create)
+      @store.setx('createme', 'done!', :create)
     end
   end
 
+  def test_replace_option
+    assert_raise Errno::ENOATTR do
+      @store.setx('replaceme', 'done!', :replace)
+    end
+    @store.setx('replaceme', 'done!')
+    assert_nothing_raised do
+      @store.setx('replaceme', 'done!', :replace)
+    end
+  end
+
+  def test_link_nofollow
+    link_file = File.expand_path('./test_link', File.dirname(__FILE__))
+    ln_s __FILE__, link_file
+    link_store = Xtendr(link_file)
+    link_store.setx('dontfollow', 'justdont', :create, :no_follow)
+    assert_nil @store.getx('dontfollow')
+    assert_equal 'justdont', link_store.getx('dontfollow', :no_follow)
+  ensure
+    rm link_file
+  end
+
+  def test_link_follow
+    link_file = File.expand_path('./test_link', File.dirname(__FILE__))
+    ln_s __FILE__, link_file
+    link_store = Xtendr(link_file)
+    link_store.setx('dontfollow', 'justdont', :create)
+    assert_equal 'justdont', @store.getx('dontfollow')
+    assert_nil link_store.getx('dontfollow', :no_follow)
+  ensure
+    rm link_file
+  end
 end
